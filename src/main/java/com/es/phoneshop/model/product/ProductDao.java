cfb2a69bedeef55c37abdf32f5c4d0408dba607a
odlmon <dmi3yosipov@gmail.com>
2021-02-01 15:07:26 +0300

    Fix: Task 1.5 ArrayListProductDao with exceptional approach for null handling

--- src/main/java/com/es/phoneshop/model/product/ProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ProductDao.java
@@ -3,8 +3,8 @@ package com.es.phoneshop.model.product;
 import java.util.List;
 
 public interface ProductDao {
-    Product getProduct(Long id) throws ProductNotFoundException;
+    Product getProduct(Long id) throws ProductNotFoundException, NullValuePassedException;
     List<Product> findProducts();
-    void save(Product product);
+    void save(Product product) throws NullValuePassedException;
     void delete(Long id) throws ProductNotFoundException;
 }
